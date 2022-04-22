1774b1f041c62ca11835f87688b19c2480f4063a
Diego Alcantara <dalcantara7@email.arizona.edu>
2022-04-22 18:31:47 -0400

    first push

--- README.md
+++ README.md
@@ -1,72 +1,28 @@
-## Obsidian Sample Plugin
+# AutoMOC for Obsidian
 
-This is a sample plugin for Obsidian (https://obsidian.md).
+This plugin automatically imports any linked mentions into the current note that are not already in the note at the current cursor location. <br>
+When taking notes, one may forget to provide the backlink to the MOC for which a new note was linked. Over time this leads to many purely directional links. MOCs may miss many notes they are linked to purely by the user forgetting. <br>
+This resolves that issue by checking for missing linked mentions and adding them to the current note at the current cursor location.
 
-This project uses Typescript to provide type checking and documentation.
-The repo depends on the latest plugin API (obsidian.d.ts) in Typescript Definition format, which contains TSDoc comments describing what it does.
+## Usage
 
-**Note:** The Obsidian API is still in early alpha and is subject to change at any time!
+After enabling this plugin, place your cursor in an editable note where you want the links to be added. <br>
+Run the plugin either through the command pallette, keyboard shortcut (must be mapped), or ribbon button.
 
-This sample plugin demonstrates some of the basic functionality the plugin API can do.
-- Changes the default font color to red using `styles.css`.
-- Adds a ribbon icon, which shows a Notice when clicked.
-- Adds a command "Open Sample Modal" which opens a Modal.
-- Adds a plugin setting tab to the settings page.
-- Registers a global click event and output 'click' to the console.
-- Registers a global interval which logs 'setInterval' to the console.
+## Known issues
 
-### First time developing plugins?
+1. This plugin requires that you enable the core backlink plugin and turn on the "Backlink in document" setting for the plugin. <br>
+   The plugin uses this backlink pane to find the linked mentions.
+2. The backlink pane only renders up to 13 notes at a time by default. That means any notes below 13 will not be seen by the plugin and may not be added. <br>
+   To get around this, first place the cursor in the note where you would link the links to be added. Then scroll all the way to the bottom of the backlink pane, then run the plugin either through a keyboard shortcut or the ribbon icon. <br>
+   <br>
+   This is, unfortunately, a necessary evil as the Obsidian API does not explicitly expose the backlinks, so the links have to be gathered from the raw UI. I will be suggesting this as an improvement to the plugin API.
+3. This plugin does support the use of the Sliding Panes plugin and will put the new links in the currently active note.
 
-Quick starting guide for new plugin devs:
+## Pricing
 
-- Make a copy of this repo as a template with the "Use this template" button (login to GitHub if you don't see it).
-- Clone your repo to a local development folder. For convenience, you can place this folder in your `.obsidian/plugins/your-plugin-name` folder.
-- Install NodeJS, then run `npm i` in the command line under your repo folder.
-- Run `npm run dev` to compile your plugin from `main.ts` to `main.js`.
-- Make changes to `main.ts` (or create new `.ts` files). Those changes should be automatically compiled into `main.js`.
-- Reload Obsidian to load the new version of your plugin.
-- Enable plugin in settings window.
-- For updates to the Obsidian API run `npm update` in the command line under your repo folder.
+This plugin is provided to everyone for free, however if you would like to say thanks or help support continued development, feel free to send a little my way through one of the following methods:
 
-### Releasing new releases
+## Notes
 
-- Update your `manifest.json` with your new version number, such as `1.0.1`, and the minimum Obsidian version required for your latest release.
-- Update your `versions.json` file with `"new-plugin-version": "minimum-obsidian-version"` so older versions of Obsidian can download an older version of your plugin that's compatible.
-- Create new GitHub release using your new version number as the "Tag version". Use the exact version number, don't include a prefix `v`. See here for an example: https://github.com/obsidianmd/obsidian-sample-plugin/releases
-- Upload the files `manifest.json`, `main.js`, `styles.css` as binary attachments. Note: The manifest.json file must be in two places, first the root path of your repository and also in the release.
-- Publish the release.
-
-> You can simplify the version bump process by running `npm version patch`, `npm version minor` or `npm version major` after updating `minAppVersion` manually in `manifest.json`.
-> The command will bump version in `manifest.json` and `package.json`, and add the entry for the new version to `versions.json`
-
-### Adding your plugin to the community plugin list
-
-- Check https://github.com/obsidianmd/obsidian-releases/blob/master/plugin-review.md
-- Publish an initial version.
-- Make sure you have a `README.md` file in the root of your repo.
-- Make a pull request at https://github.com/obsidianmd/obsidian-releases to add your plugin.
-
-### How to use
-
-- Clone this repo.
-- `npm i` or `yarn` to install dependencies
-- `npm run dev` to start compilation in watch mode.
-
-### Manually installing the plugin
-
-- Copy over `main.js`, `styles.css`, `manifest.json` to your vault `VaultFolder/.obsidian/plugins/your-plugin-id/`.
-
-### Improve code quality with eslint (optional)
-- [ESLint](https://eslint.org/) is a tool that analyzes your code to quickly find problems. You can run ESLint against your plugin to find common bugs and ways to improve your code. 
-- To use eslint with this project, make sure to install eslint from terminal:
-  - `npm install -g eslint`
-- To use eslint to analyze this project use this command:
-  - `eslint main.ts`
-  - eslint will then create a report with suggestions for code improvement by file and line number.
-- If your source code is in a folder, such as `src`, you can use eslint with this command to analyze all files in that folder:
-  - `eslint .\src\`
-
-
-### API Documentation
-
-See https://github.com/obsidianmd/obsidian-api
+This plugin has not been tested on mobile
--- main.ts
+++ main.ts
@@ -1,137 +1,172 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App,
+	Editor,
+	MarkdownView,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+} from "obsidian";
 
-// Remember to rename these classes and interfaces!
+// interface AutoMOCSettings {
+// 	mySetting: string;
+// }
 
-interface MyPluginSettings {
-	mySetting: string;
-}
+// const DEFAULT_SETTINGS: AutoMOCSettings = {
+// 	mySetting: "Uncategorized",
+// };
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class AutoMOC extends Plugin {
+	// settings: AutoMOCSettings;
 
 	async onload() {
-		await this.loadSettings();
+		// await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+		const ribbonIconEl = this.addRibbonIcon(
+			"sheets-in-box",
+			"AutoMOC",
+			(evt: MouseEvent) => {
+				runAutoMOC();
 			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
+		);
+
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
+			id: "add-missing-linked-mentions",
+			name: "Add missing linked mentions at cursor position",
 			checkCallback: (checking: boolean) => {
 				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
+				const markdownView =
+					this.app.workspace.getActiveViewOfType(MarkdownView);
 				if (markdownView) {
 					// If checking is true, we're simply "checking" if the command can be run.
 					// If checking is false, then we want to actually perform the operation.
 					if (!checking) {
-						new SampleModal(this.app).open();
+						runAutoMOC();
 					}
 
 					// This command will only show up in Command Palette when the check function returns true
 					return true;
 				}
-			}
+			},
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		// this.addSettingTab(new AutoMOCSettingTab(this.app, this));
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+		// this.registerDomEvent(document, "click", (evt: MouseEvent) => {
+		// 	console.log("click", evt);
+		// });
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.registerInterval(
+			window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000)
+		);
 	}
 
-	onunload() {
+	onunload() {}
 
-	}
+	// async loadSettings() {
+	// 	this.settings = Object.assign(
+	// 		{},
+	// 		DEFAULT_SETTINGS,
+	// 		await this.loadData()
+	// 	);
+	// }
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+	// async saveSettings() {
+	// 	await this.saveData(this.settings);
+	// }
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+function getPresentLinks() {
+	const file = this.app.workspace.getActiveFile();
+	const path = file.path;
+	const linksStruct = this.app.metadataCache.getCache(path).links;
+
+	let links = [];
+	for (const link of linksStruct) {
+		links.push(link["link"]);
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+	return links;
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+function getLinkedMentions() {
+	//gets the linked mentions from the backlinks panel
+	const workspaceContainer = this.app.workspace.containerEl;
+	const activePane = workspaceContainer.getElementsByClassName(
+		"workspace-leaf mod-active"
+	)[0];
+	const backlinkPane = activePane.getElementsByClassName("backlink-pane")[0];
+	const linkedMentionsContainer = backlinkPane.children[1]; //0th element is "Linked Mentions", 1st is the actual linked mentions container, 2nd is "Unlinked Mentions", 3rd is the actual unlinked mentions container
+	const linkedMentions = linkedMentionsContainer.children[0].children;
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
+	let linkNamesList = [];
+	for (let i = 0; i < linkedMentions.length; i++) {
+		let inner = linkedMentions[i].children;
+		if (inner.length > 0) {
+			let linkName = inner[0].innerText
+				.slice(0, -1)
+				.replace(/(\r\n|\n|\r)/gm, "");
+			linkNamesList.push(linkName);
+		}
 	}
 
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+	return linkNamesList;
 }
+
+function addMissingLinks(presentLinks, allLinkedMentions) {
+	//checks for missing links and adds them
+	let addFlag = false;
+	for (const name of allLinkedMentions) {
+		if (!presentLinks.includes(name)) {
+			addFlag = true;
+			let view = this.app.workspace.getActiveViewOfType(MarkdownView);
+			view.editor.replaceSelection("[[" + name + "]]\n");
+		}
+	}
+
+	if (!addFlag) new Notice("No new links found");
+}
+
+function runAutoMOC() {
+	new Notice("Linking mentions");
+	const presentLinks = getPresentLinks(); // links already in the document
+	const linkedMentions = getLinkedMentions(); // all linked mentions even those not present
+
+	addMissingLinks(presentLinks, linkedMentions);
+}
+
+// class AutoMOCSettingTab extends PluginSettingTab {
+// 	plugin: AutoMOC;
+
+// 	constructor(app: App, plugin: AutoMOC) {
+// 		super(app, plugin);
+// 		this.plugin = plugin;
+// 	}
+
+// 	display(): void {
+// 		const { containerEl } = this;
+
+// 		containerEl.empty();
+
+// 		new Setting(containerEl)
+// 			.setName("Link Location")
+// 			.setDesc(
+// 				"The keyword under which your unlinked mentions will be added"
+// 			)
+// 			.addText((text) =>
+// 				text
+// 					.setPlaceholder("Uncategorized")
+// 					// .setValue(this.plugin.settings.mySetting)
+// 					.onChange(async (value) => {
+// 						if (!value) value = "Uncategorized"; //revert to default if no value set
+// 						this.plugin.settings.mySetting = value;
+// 						await this.plugin.saveSettings();
+// 						console.log("Link keyword: " + value);
+// 					})
+// 			);
+// 	}
+// }
